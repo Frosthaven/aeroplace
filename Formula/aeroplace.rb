121e02cb0a6ed7429f47eac781ee080a61f42f96
Frosthaven <frosthaven@thedragon.dev>
2025-02-05 16:46:04 -0500

    0.0.2 update

--- Formula/aeroplace.rb
+++ Formula/aeroplace.rb
@@ -1,35 +1,21 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Aeroplace < Formula
   desc "Helper utility for generating deterministic Aerospace application layouts at launch"
   homepage "https://github.com/Frosthaven/aeroplace"
-  url "https://github.com/Frosthaven/aeroplace/archive/refs/tags/v0.0.1.zip"
+  url "https://github.com/Frosthaven/aeroplace/archive/refs/tags/v0.0.2.zip"
+
+  # regenerate SHA256 with this format for your release download:
+  # curl -L https://github.com/Frosthaven/aeroplace/archive/refs/tags/v0.0.2.zip | shasum -a 256
   sha256 "aadf40cb211e2afa250580096cc0804fcebdfb73b8119ddbc75147a5eebae18e"
+  
   license "MIT"
 
   depends_on "aerospace"
-
-  # Additional dependency
-  # resource "" do
-  #   url ""
-  #   sha256 ""
-  # end
 
   def install
     bin.install "src/run.sh" => "aeroplace"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test aeroplace`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
     system "false"
   end
 end
