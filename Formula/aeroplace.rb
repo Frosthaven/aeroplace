be839462195da7dff06d639a94952a2fd1ceb7b3
Frosthaven <frosthaven@thedragon.dev>
2025-02-05 18:12:13 -0500

    updated formula

--- Formula/aeroplace.rb
+++ Formula/aeroplace.rb
@@ -5,7 +5,7 @@ class Aeroplace < Formula
 
   # regenerate SHA256 with this format for your release download:
   # curl -L https://github.com/Frosthaven/aeroplace/archive/refs/tags/v0.0.2.tar.gz | shasum -a 256
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed" 
+  sha256 "bf63945e1e5bef31fa437170e8f6384d3fd53696f78a24ac64e3e6ac7d74c7e4" 
   
   license "MIT"
 
