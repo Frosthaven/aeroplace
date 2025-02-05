56ae896ba3ce76818a119b3d5f6189d6afe1f0f5
Frosthaven <frosthaven@thedragon.dev>
2025-02-05 16:49:19 -0500

    Formula update

--- Formula/aeroplace.rb
+++ Formula/aeroplace.rb
@@ -1,11 +1,11 @@
 class Aeroplace < Formula
   desc "Helper utility for generating deterministic Aerospace application layouts at launch"
   homepage "https://github.com/Frosthaven/aeroplace"
-  url "https://github.com/Frosthaven/aeroplace/archive/refs/tags/v0.0.2.zip"
+  url "https://github.com/Frosthaven/aeroplace/archive/refs/tags/v0.0.2.tar.gz"
 
   # regenerate SHA256 with this format for your release download:
-  # curl -L https://github.com/Frosthaven/aeroplace/archive/refs/tags/v0.0.2.zip | shasum -a 256
-  sha256 "aadf40cb211e2afa250580096cc0804fcebdfb73b8119ddbc75147a5eebae18e"
+  # curl -L https://github.com/Frosthaven/aeroplace/archive/refs/tags/v0.0.2.tar.gz | shasum -a 256
+  sha256 "6b2066319823a01bad845d01b8fc4570f7bcd314492dd99974596bd44ccd0f1b" 
   
   license "MIT"
 
